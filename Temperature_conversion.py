1eb16784bbe6a6864078a6391c8f76743b14083c
R Thanuj <116823527+Thanujj@users.noreply.github.com>
2023-04-01 13:38:01 +0530

    Delete Temperature_conversion.py

--- Temperature_conversion.py
+++
@@ -1,9 +0,0 @@
-# CONVERTING FAHRIEN HEAT TEMPERATURE TO CELSIUS SCALE
-print("ENTER Fahrien temperature")
-
-# The Fahrien temperature will be given by the user
-fahrien = float(input())
-celsius = float(5) / 9 * (fahrien - 32)
-
-# Here the system will give Celsius temperature
-print("The Celsius temperature " + str(celsius))
