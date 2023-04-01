ce220ff7e8a027a746c19abffe5c72730f3b1e1b
R Thanuj <116823527+Thanujj@users.noreply.github.com>
2023-04-01 13:36:41 +0530

    Delete Sum_Of_n_numbers.py

--- Sum_Of_n_numbers.py
+++
@@ -1,9 +0,0 @@
-# The system get number from user and process then sum
-print("Enter the number")
-n = int(input())
-sum = 0
-i = 1
-while i <= n:
-    sum = i + sum
-    i = i + 1
-print("The sum is  " + str(sum))
